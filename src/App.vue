536d21a95f758dac5358c1a31a12d30b74d321d0
coderxu <xu_dreamer@sina.com>
2025-07-02 23:53:38 +0800

    去掉根组件app上的样式 auto_coder_000000000010_chat_action.yml

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,3 @@
   </Transition>
 </template>
 
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
